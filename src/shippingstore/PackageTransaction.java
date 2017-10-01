e8e2823e22b9516b4912d08b51aeccf77b7a4b02
Kentessa F. Goldstein <k_f145@txstate.edu>
2017-09-30 21:03:13 -0500

    added new class PackageTransaction

---
+++ src/shippingstore/PackageTransaction.java
@@ -0,0 +1,11 @@
+package shippingstore;
+
+import java.util.Date;
+
+public class PackageTransactions {
+    private Date shippingDate;
+    private Date deliveryDate;
+    private float shippingCost;
+
+    // CONTINUE HERE
+}
